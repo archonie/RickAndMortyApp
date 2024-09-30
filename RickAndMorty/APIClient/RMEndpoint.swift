ab3be2d82e952e4f734d13fc7b2f8d41bb36c90a
Doğan Ensar Papuçcuoğlu <ensarpabuccuoglu@gmail.com>
2024-10-01 00:51:20 +0300

    Added docstrings and API related files

---
+++ RickAndMorty/APIClient/RMEndpoint.swift
@@ -0,0 +1,18 @@
+//
+//  RMEndpoint.swift
+//  RickAndMorty
+//
+//  Created by Doğan Ensar Papuçcuoğlu on 1.10.2024.
+//
+
+import Foundation
+
+/// Represents unique API endpoints
+@frozen enum RMEndpoint: String{
+    /// Endpoint to get character info
+    case character
+    /// Endpoint to get location info
+    case location
+    /// Endpoint to get episode info
+    case episode
+}
---
+++ RickAndMorty/APIClient/RMRequest.swift
@@ -0,0 +1,13 @@
+//
+//  RMRequest.swift
+//  RickAndMorty
+//
+//  Created by Doğan Ensar Papuçcuoğlu on 1.10.2024.
+//
+
+import Foundation
+
+/// Object that represents a single API Call
+final class RMRequest {
+    
+}
