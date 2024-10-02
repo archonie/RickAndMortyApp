e82638863ed82761b5f21b33c5db4d1f5cf28f7a
Doğan Ensar Papuçcuoğlu <ensarpabuccuoglu@gmail.com>
2024-10-02 05:19:12 +0300

    Complete API Cache Layer

---
+++ RickAndMorty/ViewModels/EpisodeDetails/RMEpisodeDetailViewViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  RMEpisodeDetailViewViewModel.swift
+//  RickAndMorty
+//
+//  Created by Doğan Ensar Papuçcuoğlu on 2.10.2024.
+//
+
+import Foundation
+
+final class RMEpisodeDetailViewViewModel {
+    
+    private let endpointUrl: URL?
+    
+    init(endpointUrl: URL?) {
+        self.endpointUrl = endpointUrl
+    }
+}
